ec1613f5af4479b3a4c75b5550f68234a1da671e
Nate Ryan <nathanieljryan1994@gmail.com>
2023-03-08 14:04:37 -0600

    working with many commands

--- chatgpt.py
+++ chatgpt.py
@@ -9,15 +9,18 @@ dotenv.load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 copy_command = "pbcopy"
 
-def switch_case(flag):
+def switch_case(index):
     switcher = {
+        "--cmd": "Write a one line bash command for this task between two backticks: ",
+        "--search": "Search the web for this and return a URL: ",
+        "--comment": "Write a comment for this code: ",
         "--chat": "",
-        "default": "Write a one line bash command for this task: "
+        "default": ""
     }
-    return switcher.get(flag, switcher["default"])
+    return switcher.get(index, switcher["default"])
 
-question = sys.argv[1]
-formatted_question = switch_case("") + question
+(flag, question) = sys.argv[1].split(maxsplit=1)
+formatted_question = switch_case(flag) + question
 
 response = openai.ChatCompletion.create(
     model="gpt-3.5-turbo",
@@ -28,20 +31,20 @@ response = openai.ChatCompletion.create(
 )
 
 result = ''
+reply = ''
 for choice in response.choices:
     result += choice.message.content
 
-# Using a regular expression to extract the one-line bash command from the chatbot's response
-test = re.search(r"(```\n)(.*)(\n```)", result)
-if test:
-	# If a match was found, printing the command and copying it to the clipboard
-	result = test.group(2)
-	print("Copied to clipboard:  " + result)
-else:
-	# If no match was found, indicating that the answer could not be parsed
-	print(f"Could not Parse answer:   \n{'-'*50}\n{result}\n{'-'*50}")
+if flag == "--cmd":
+    try :
+        reply = re.search(r'`(.*)`', result).group(1).replace('`', '')
+    except AttributeError:
+        reply = "error"
 
-# Using the subprocess library to copy the command to the clipboard
-subprocess.run(copy_command, text=True, input=result)
+if flag == "--search":
+    try :
+        reply = re.search(r'(?P<url>https?://[^\s]+)', result).group("url")
+    except AttributeError:
+        reply = "error"
 
 print(result)
